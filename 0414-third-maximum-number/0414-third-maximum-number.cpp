42ac843d07fc42fd200f0b45a1de639a65f8b41a
Hoang Nguyen <110825161+hoangnguyen3110@users.noreply.github.com>
2026-04-15 21:06:43 +0700

    Time: 0 ms (100%), Space: 13 MB (51.17%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long long max1= numeric_limits<long long>::min();
+        long long max2 = numeric_limits<long long>::min();
+        long long max3 = numeric_limits<long long>::min();
+        for (int num : nums) {
+            if (num == max1 || num == max2 || num == max3) {
+                continue;
+            }
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3) {
+                max3 = num;
+            }
+
+        }
+        if (max3 == numeric_limits<long long>::min()) {
+            return max1;
+        }
+        return max3;
+    }
+};
